8386573205ea5da60437998759dad223c338651e
Laksh <lakshtaneja@yahoo.com>
2025-06-02 19:12:58 +0530

    implementing singly linked list

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args){
-        DynamicArray da = new DynamicArray();
+
+        // trying out linked list features
+
+        SinglyLinkedList sl = new SinglyLinkedList();
+        sl.insertHead(1);
+        sl.insertTail(2);
+        sl.insertHead(0);
+
+        sl.remove(1);
+
+        System.out.println(sl.get(0));
+
+
+        System.out.println(sl.getValues());
 
     }
 }
