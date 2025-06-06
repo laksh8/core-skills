3f55c270cc7b9a46f34747af0377c7d5af70ff21
Laksh <lakshtaneja@yahoo.com>
2025-06-06 21:41:24 +0530

    implement heapification

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        // trying out hash map
+        // trying out heap
 
-        HashedMap hm = new HashedMap(10);
+        int[] arr = {30, 20, 10, 5, 4, 3, 2};
 
-        hm.insert(5, 234);
-        hm.insert(15, 233);
+        Heaped hp = new Heaped(arr);
 
-        hm.insert(5, 123);
-        hm.insert(15, 234);
-
-        System.out.println("Size: "+ hm.getSize());
-        System.out.println("Arr Length: "+ hm.getArrLength());
-        System.out.println("Load Factor: "+ (float)hm.getSize()/hm.getArrLength());
-
-        for (int i = 0; i < 7; i++){
-            hm.insert(i, 45);
-        }
-
-        System.out.println("Size: "+ hm.getSize());
-        System.out.println("Arr Length: "+ hm.getArrLength());
-        System.out.println("Load Factor: "+ (float)hm.getSize()/hm.getArrLength());
-
-        hm.displayMap();
-
-        hm.insert(45, 32);
-
-        System.out.println("Size: "+ hm.getSize());
-        System.out.println("Arr Length: "+ hm.getArrLength());
-        System.out.println("Load Factor: "+ (float)hm.getSize()/hm.getArrLength());
-
-        hm.displayMap();
-
-        hm.remove(15);
-        hm.remove(14);
-
-        System.out.println(hm.get(5));
-        System.out.println(hm.get(4));
+        hp.displayHeap();
 
     }
+
 }
