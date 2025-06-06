89d6bd461b3b084dc81057c80bbc3de4bb8c62cf
Laksh <lakshtaneja@yahoo.com>
2025-06-07 01:16:09 +0530

    implement graph

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
-        // trying out heap
+        // trying out graph
 
-        int[] arr = {30, 20, 10, 5, 4, 3, 2};
+        Graphed gp = new Graphed();
 
-        Heaped hp = new Heaped(arr);
+        gp.addEdge("A", "B", 32);
+        gp.addEdge("A", "C", 3);
+        gp.addEdge("C", "B", 2);
+        gp.addEdge("D", "B", 332);
+        gp.addEdge("E", "B", 12);
+        gp.addEdge("A", "D", 456);
+        gp.addEdge("A", "E", 44);
+        gp.addEdge("B", "A", 35);
+        gp.addEdge("D", "A", 36);
+        gp.addEdge("C", "A", 31);
 
-        hp.displayHeap();
+        gp.printGraph();
+
+        gp.removeEdge("A", "B");
+
+        System.out.println("\n " + gp.getWeight("A", "E") + "\n");
+
+        gp.printGraph();
 
     }
 
